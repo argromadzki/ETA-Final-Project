61e851fe6df3a7c1fd49997824d82be9c01b5007
argromadzki <41020899+argromadzki@users.noreply.github.com>
2019-05-09 15:54:16 -0400

    forgot to save "customize stopwords" before commit

--- UVA Code/customize_stopwords.py
+++ UVA Code/customize_stopwords.py
@@ -2,13 +2,29 @@
 import os
 import pandas as pd
 import nltk
+import sqlite3
+import regex
 
 os.getcwd()
 extra_stopwords_file = open('extra_stopwords.txt', 'r').readlines()
 
-extra_stopwords = pd.DataFrame(extra_stopwords_file).iloc[:,0].apply((lambda x: x.strip('\n')))
+extra_stopwords = list(pd.DataFrame(extra_stopwords_file).iloc[:,0].apply((lambda x: x.strip('\n'))))
 
 stopwords = set(nltk.corpus.stopwords.words('english')  + extra_stopwords) # add extra stopwords piece within the set if defined
 
 sw = pd.DataFrame({'x':1}, index=stopwords)
-# vocab['stop'] = vocab.term_str.map(sw.x).fillna(0).astype('int')
+
+os.chdir('../../Data/Reduced')
+os.getcwd()
+
+
+with sqlite3.connect('WSJ-processed-full.db') as db:
+    V = pd.read_sql('SELECT * FROM vocab', db, index_col='term_id')
+
+
+V['stop'] = V.term_str.map(sw.x).fillna(0).astype('int')
+
+with sqlite3.connect('WSJ-processed-full.db') as db:
+    V.to_sql('vocab', db, if_exists='replace', index=True)
+
+
